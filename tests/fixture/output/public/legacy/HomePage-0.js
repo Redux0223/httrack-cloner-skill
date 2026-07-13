1bf2c507ad657e5f2ab87c7b0032a7f6861ef0fc
Redux0223 <171701490+Redux0223@users.noreply.github.com>
2026-07-13 16:33:11 +0800

    chore: keep generated fixtures out of release

--- tests/fixture/output/public/legacy/HomePage-0.js
+++
@@ -1,8 +0,0 @@
-
-      window._CACHE_ = "123";
-      var bundleName = "constructed";
-      var bundlePath = "assets/" + bundleName + "." + window._CACHE_ + ".js";
-      var bundleScript = document.createElement("script");
-      bundleScript.src = bundlePath;
-      document.head.appendChild(bundleScript);
-    
